942bbc732b802917d3b61167d0286cbcb0de29a4
Pavel Yastremsky <goodieart@gmail.com>
2022-09-29 00:29:57 +0300

    task_021 completed

---
+++ task_021/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+float temp = 0, sum = 0;
+
+Clear();
+WriteLine("Введите координаты точек A,B: ");
+
+for(char i = 'x'; i <= 'z'; i++)
+{    
+    temp = 0;
+    for(int j = 1; j > -2; j -= 2)
+    {
+        Write($"{i}_{(j == 1?"A":"B")}: ");
+        temp += float.Parse(ReadLine()!) * j;
+    }
+    sum += temp * temp;
+}
+
+WriteLine($"{Math.Sqrt(sum):f2}");
