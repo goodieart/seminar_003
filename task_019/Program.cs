dd5406eae81fc1d17541a5b5a411b7cc520ae570
Pavel Yastremsky <goodieart@gmail.com>
2022-09-28 23:20:30 +0300

    task_019 completed

---
+++ task_019/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+Clear();
+Write("Введите пятизначное число: ");
+int num = int.Parse(ReadLine()!);
+int new_num = 0;
+
+for(int i = num; i > 0; i /= 10) new_num = new_num * 10 + i % 10;
+
+Write($"Это число - {(num == new_num?"":"не ")}палиндром");
